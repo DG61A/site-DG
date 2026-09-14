34381c1c4b23890a30c19e637410da933d80b04d
DG61A <magaotvs6@gmail.com>
2026-09-14 08:48:10 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,1 @@
+window.confirm("Nosso site ultiliza Cookies. Ao clicar em OK você concorda com o uso do Cookies em nosso site");
---
+++ javacalcular.js
@@ -0,0 +1,13 @@
+function  calcularFRETE(event){
+    event.preventDefault(); 
+    var nuumero = domcument.getElementById('numero').value;
+    const selecionar= document.querySelector('#options');
+    console.log(selecionar);
+    var valor =selecionar;
+}
+
+
+
+
+document.getElementById(' resultado '). style.display="none"; 
+document.getElementById('resultadoFrete').addEventListener(submit, calcularFRETE );
